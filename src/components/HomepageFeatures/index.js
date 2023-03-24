737a76e2d362add64650462057838b76f0f766e0
j-sobi <perhapss.2023@gmail.com>
2023-03-25 04:34:32 +0800

    update index.js img

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -38,11 +38,11 @@ const FeatureList = [
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({img, title, description}) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
-        <img src={img} />
+        <img src={img} className={styles.featureSvg} role="img" />
       </div>
       <div className="text--center padding-horiz--md">
         <h3>{title}</h3>
