948e824fe04b6867f9c955ce0e73d4c43605b626
j-sobi <perhapss.2023@gmail.com>
2023-03-25 00:11:46 +0800

    edit url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,7 +78,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/j-sobi',
             label: 'GitHub',
             position: 'right',
           },
@@ -122,12 +122,12 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/j-sobi',
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} My Blog, Built by 6$.`,
       },
       prism: {
         theme: lightCodeTheme,
