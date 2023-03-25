24c8dcf53b80f4fd333177d9e9880347515bbdef
j-sobi <perhapss.2023@gmail.com>
2023-03-25 20:30:43 +0800

    update

--- src/components/GiscusComponent.js
+++ src/components/GiscusComponent.js
@@ -1,26 +1,24 @@
 //add comments
-import Giscus from "@giscus/react";
+import { Giscus } from "@giscus/react";
+
 import { useColorMode } from '@docusaurus/theme-common';
 
 //add comments
-export default function GiscusComponent() {
-    const { colorMode } = useColorMode();
-  
-    return (
-      <script src="https://giscus.app/client.js"
-              data-repo="j-sobi/co2"
-              data-repo-id="R_kgDOJNx76w"
-              data-category="Announcements"
-              data-category-id="DIC_kwDOJNx7684CVIwh"
-              data-mapping="url"
-              data-strict="0"
-              data-reactions-enabled="1"
-              data-emit-metadata="0"
-              data-input-position="bottom"
-              data-theme="preferred_color_scheme"
-              data-lang="zh-CN"
-              crossorigin="anonymous"
-              async>
-      </script>
-    );
-  }
+export default function Comment() {
+  return (
+    <Giscus
+      repo="j-sobi/co2"
+      repoId="R_kgDOJNx76w"
+      category="Announcements"
+      categoryId="DIC_kwDOJNx7684CVIwh"
+      mapping="pathname"
+      data-strict="0"
+      data-reactions-enabled="1"
+      data-emit-metadata="0"
+      data-input-position="bottom"
+      reactionsEnabled="0"
+      emitMetadata="0"
+      theme="dark"
+    />
+  );
+}
